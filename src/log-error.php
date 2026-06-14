3e40a0286fb914d65dfb89243703671a9fbfde26
Joseph Scott <joseph@josephscott.org>
2026-06-13 23:17:38 -0600

    Tighten up log_error()

--- src/log-error.php
+++ src/log-error.php
@@ -2,12 +2,11 @@
 declare( strict_types = 1 );
 
 function log_error( mixed $data ) : void {
-	$msg = '';
+	$msg = $data;
 
 	if ( ! is_string( $data ) ) {
 		$msg = print_r( $data, true );
 	}
 
-	echo $msg;
 	error_log( $msg );
 }
